bfbce703c68c3e4c39850a5d3f5d37765a837540
Cristelly-LO <cristelaltarejos@gmail.com>
2026-07-15 12:29:07 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,79 @@
+// ==========================
+// ELEMENTS
+// ==========================
+
+const lockScreen = document.getElementById("lockScreen");
+const faceScreen = document.getElementById("faceScreen");
+const updateScreen = document.getElementById("updateScreen");
+
+const faceBar = document.getElementById("faceBar");
+const faceStatus = document.getElementById("faceStatus");
+
+const clock = document.querySelector(".clock");
+const topTime = document.getElementById("time");
+const date = document.querySelector(".date");
+
+// ==========================
+// CHANGE SCREEN
+// ==========================
+
+function showScreen(screen){
+
+    document.querySelectorAll(".screen").forEach(s=>{
+
+        s.classList.remove("active");
+
+    });
+
+    screen.classList.add("active");
+
+}
+
+// ==========================
+// LOCK SCREEN
+// ==========================
+
+lockScreen.addEventListener("click",()=>{
+
+    showScreen(faceScreen);
+
+    setTimeout(startFaceRecognition,500);
+
+});
+
+// ==========================
+// FACE ID
+// ==========================
+
+function startFaceRecognition(){
+
+    faceBar.style.width="100%";
+
+    setTimeout(()=>{
+
+        faceStatus.innerHTML="✔ Face Recognized";
+
+    },3000);
+
+    setTimeout(()=>{
+
+        clock.innerHTML="12:00 AM";
+        topTime.innerHTML="12:00 AM";
+
+        date.innerHTML="Thursday, July 16";
+
+    },3300);
+
+    setTimeout(()=>{
+
+        faceStatus.innerHTML="🎉 Happy Birthday, Raymart!";
+
+    },4200);
+
+    setTimeout(()=>{
+
+        showScreen(updateScreen);
+
+    },5600);
+
+}
